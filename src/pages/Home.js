90b00dda5960db7bb6c84ac772aff31056499a20
DoVuDung <vudungoik2016@gmail.com>
2021-09-25 22:57:54 +0700

    Home ui

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,83 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import styles from './Home.module.scss'
 import Button from '../components/UI/Button/Button'
 import Card from '../components/UI/Card/Card'
 import iconAddCircle from '../assets/images/add_circle.png'
+import iconKey from '../assets/images/keyboard_tab.png'
 function Home(props) {
   return (
     <div className={styles.container}>
       <div className={styles.container__workspaces}>
-        <div className={['styles.container__workspaces--head']}>
-          <h3>Your WorkSpaces</h3>
-          <Button><img src={iconAddCircle} alt="add icon"/>Room</Button>
+        <div className={styles['container__workspaces--head']}>
+          <h1>Your WorkSpaces</h1>
+          <Button className={styles.button__tools}><img src={iconAddCircle} alt="add icon" />Room</Button>
         </div>
-        <Card>Your own room</Card>
+        <Card className={`${styles.room} ${styles['own__room--blue']}`}>Your own room</Card>
       </div>
       <div className={styles.container__recent}>
-        <div className={['styles.container__recent--chidls']}>
-          <Card key={props.roomId}>Your created room 1</Card>
-          <Card key={props.roomId}>Your created room 2</Card>
-          <Card key={props.roomId}>Your created room 3</Card>
-          <Card key={props.roomId}>Your created room 4</Card>
+        <div className={styles['container__recent--chidls']}>
+          <div className={styles['container__recent--chidl']}>
+            <Card className={`${styles.room} ${styles['own__room--red']}`} key={props.roomId}>
+              Your created room 1
+            </Card>
+           </div> 
+            <div className={styles['container__recent--chidl']}>
+              <Card className={`${styles.room} ${styles['own__room--red']}`} key={props.roomId}>
+                Your created room 2
+              </Card>
+            </div>
+            <div className={styles['container__recent--chidl']}>
+              <Card className={`${styles.room} ${styles['own__room--red']}`} key={props.roomId}>
+                Your created room 3
+              </Card>
+            </div>
+            <div className={styles['container__recent--chidl']}>
+              <Card className={`${styles.room} ${styles['own__room--red']}`} key={props.roomId}>
+                Your created room 4
+              </Card>
+            </div>
+          </div>
         </div>
         <span className={styles['container__recent--show__rooms']}>
-          <u>Browser all your created room</u>
+            <u>Browser all your created room</u>
         </span>
+
+        <div className={`${styles.container__workspaces} ${styles.container__bottom}`}>
+        <div className={styles['container__workspaces--bottom']}>
+          <h1>Your WorkSpaces</h1>
+          <Button className={styles.button__tools}><img src={iconKey} alt="add icon" />Join</Button>
+        </div>
+       
       </div>
-    </div>
+      <div className={styles.container__recent}>
+        <div className={styles['container__recent--chidls']}>
+          <div className={styles['container__recent--chidl']}>
+            <Card className={`${styles.room} ${styles['own__room--organ']}`} key={props.roomId}>
+              Your created room 1
+            </Card>
+           </div> 
+            <div className={styles['container__recent--chidl']}>
+              <Card className={`${styles.room} ${styles['own__room--organ']}`} key={props.roomId}>
+                Your created room 2
+              </Card>
+            </div>
+            <div className={styles['container__recent--chidl']}>
+              <Card className={`${styles.room} ${styles['own__room--organ']}`} key={props.roomId}>
+                Your created room 3
+              </Card>
+            </div>
+            <div className={styles['container__recent--chidl']}>
+              <Card className={`${styles.room} ${styles['own__room--organ']}`} key={props.roomId}>
+                Your created room 4
+              </Card>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    
+
+
   )
 }
-
 export default Home
