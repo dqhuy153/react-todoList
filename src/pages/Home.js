bf63978155215319fd9b6308475dd77db04a35ab
DoVuDung <vudungoik2016@gmail.com>
2021-09-25 19:35:51 +0700

    adding reset css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,18 +2,18 @@ import React from 'react'
 import styles from './Home.module.scss'
 import Button from '../components/UI/Button/Button'
 import Card from '../components/UI/Card/Card'
-
+import iconAddCircle from '../assets/images/add_circle.png'
 function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.container__workspaces}>
         <div className={['styles.container__workspaces--head']}>
           <h3>Your WorkSpaces</h3>
-          <Button>Room</Button>
+          <Button><img src={iconAddCircle} alt="add icon"/>Room</Button>
         </div>
       </div>
       <div className={styles.container__recent}>
-
+        
       </div>
     </div>
   )
